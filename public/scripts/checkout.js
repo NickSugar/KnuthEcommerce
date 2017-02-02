9559761d00fc6573acdd920c330c857f88774e24
Paul Torres <torrepaul@gmail.com>
2017-02-02 16:37:36 -0700

    commiting changes made to my branch on thursday 2 2 17

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -15,29 +15,83 @@
 
 validator will need to run when client hits the copy button, and
 */
-
-
-
+$("#State-Shipping-1").on('change', function() {
+console.log(this.selectedIndex);
+});
 
 
 $('#purchase').on('click', function() {
-  console.log( $("#First-Name-Shipping1").val()
-);
-  formValidation();
+
+
+
+
+    if ( !ShippingValidation()) {
+    alert("please complete your shipping info")
+    }
+ //  else if (!BillingValidation()) //build this out!
+ //
+ //  else if(!CCValidation()) //build this out!
+ //
+    else {
+      alert('thank you for your purchase!')
+ }
+
 });
 
-var firstName = $("#First-Name-Shipping1").val()
-
-console.log(firstName);
 
 
+function ShippingValidation() {
+  // instead of console logs or a bunch of alerts, create a variable, increment it, at end of function, log an alert of form complete if = 0 or form incomplete if greater than 0!
+  console.log($('#Zip-Shipping1').val().length);
 
-function formValidation() {
-    if( $('#First-Name-Shipping1').val() === 'undefined') {
-      console.log("need info");
+  $("#State-Shipping-1").on('change', function() {
+  console.log(this.selectedIndex);
+  });
+  if ( $('#First-Name-Shipping1').val() === '') {
+    /*this will need to change...maybe have a counter variable and then display a batch message if there's more than one field blank? build out validator first!*/
+      return false;
     }
-    else {
-      console.log("entered info");
+    else if( $('#Last-Name-Shipping1').val() === '') {
+     return false;
     }
+    else if( $('#Address-Line-1-Shipping1').val() === '') {
+    return false;
+    }
+    else if( $("#State-Shipping-1").attr('selectedIndex') > 0) {
+    return false;
+    }
+    //so close!
+    else if ( $('#Zip-Shipping1').val().length < 5 || $('#Zip-Shipping1').val().length > 9) {
+    return false;
+    }
+//console.log('it passes!');
+return true
+    //scracth the counter idea, return false if any condition met, then in else,
 }
-})
+
+//onchange to run check, then fill out
+
+
+function cCValidation () {}
+
+
+
+
+
+// $('#Zip-Shipping1').val().length >= 5 && $('#Zip-Shipping1').val().length <= 9 )
+
+
+// function formValidation() {
+//     if( $('#First-Name-Shipping1').val() === 'undefined') {
+//       console.log("need info");
+//     }
+//     else {
+//       console.log("entered info");
+//     }
+// }
+// })
+//
+// $('#purchase').on('click', function() {
+//
+//   formValidation();
+// });
