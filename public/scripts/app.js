fec0c41449cd6d223eb19efd8524df678fc78d34
Paul Torres <torrepaul@gmail.com>
2017-02-01 17:18:46 -0700

    all worked completed ao 530 on 2 1 17

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,13 @@
 
 //think! you'll have a few different submit buttons, so you'll need button potentially multiple button classes, including ('copy shipping to billing' and 'purchase')
 
+/* here's all of the code for the email validator
+! one last potential issue- form clears whether email is accepted or not, may need to add functionality!*/
 
-$('button').on('click', function() {
-  var emailAddress = $('input[email="email address"]').val();
+$('#submit-email').on('click', function() {
+  var emailAddress = $('#email-input').val();
+
+  // var emailAddress = $('input[email="email address"]').val();
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (emailAddress.length === 0) {
@@ -18,6 +22,37 @@ $('button').on('click', function() {
 
 });
 
-// if (emailAddress === "") {
-//   alert('please enter an email address');
-// }
+/* here's where we start with the form validator JS!
+1- thinking it makes sense to have two separate forms,
+
+validator will need to run when client hits the copy button, and
+*/
+
+
+
+
+
+$('#purchase').on('click', function() {
+  console.log( $("#First-Name-Shipping1").val()
+);
+  formValidation();
+});
+
+var firstName = $("#First-Name-Shipping1").val()
+
+console.log(firstName);
+
+
+
+function formValidation() {
+    if( $('#First-Name-Shipping1').val() === 'undefined') {
+      console.log("need info");
+    }
+    else {
+      console.log("entered info");
+    }
+
+
+
+
+}
