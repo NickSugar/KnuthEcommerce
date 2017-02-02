e4b914ccec199a5557729acfdb2b13ab76dd2820
NickSugar <nicksugar1@outlook.com>
2017-02-01 17:05:32 -0700

    swapped to using arrt(src)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
 
   var productImages = [
-    'url("http://piq.codeus.net/static/media/userpics/piq_32381_400x400.png")',
-    'url("http://placehold.it/401x400")',
-    'url("http://placehold.it/402x400")',
-    'url("http://placehold.it/403x400")',
-    'url("http://placehold.it/404x400")',
-    'url("http://placehold.it/405x400")'
+    "http://placehold.it/400x400",
+    "http://placehold.it/401x400",
+    "http://placehold.it/402x400",
+    "http://placehold.it/403x400",
+    "http://placehold.it/404x400",
+    "http://placehold.it/405x400"
   ]
 
-
+  $('.carousel').attr('src', productImages[0])
   var scroll = setInterval(carouselRight, 5000)
 
   function carouselRight () {
     var firstImage = productImages.shift()
     productImages.push(firstImage)
-    $('.carousel').css('background-image', productImages[0])
+    $('.carousel').attr('src', productImages[0])
   }
   $('.rightCarousel').on('click', function(){
     clearInterval(scroll);
@@ -26,7 +26,7 @@ $(document).ready(function () {
   function carouselLeft () {
     var firstImage = productImages.pop()
     productImages.unshift(firstImage)
-    $('.carousel').css('background-image', productImages[0])
+    $('.carousel').attr('src', productImages[0])
   }
   $('.leftCarousel').on('click', function () {
     clearInterval(scroll);
