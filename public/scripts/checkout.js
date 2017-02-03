4c1c4ba2f2d954f280b2254a5761f284d19b3039
Paul Torres <torrepaul@gmail.com>
2017-02-03 12:12:33 -0700

    should be the final version

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,70 +1,48 @@
 //======================
 //Paul
 //======================
-//think! you'll have a few different submit buttons, so you'll need button potentially multiple button classes, including ('copy shipping to billing' and 'purchase')
-
-/* here's all of the code for the email validator
-! one last potential issue- form clears whether email is accepted or not, may need to add functionality!*/
-
-
-
-
-
-/* here's where we start with the form validator JS!
-1- thinking it makes sense to have two separate forms,
-
-validator will need to run when client hits the copy button, and
-*/
-$("#State-Shipping-1").on('change', function() {
-console.log(this.selectedIndex);
-});
-
 
 $('#purchase').on('click', function() {
 
-    if ( !shippingValidation()) {
-      return alert("please complete your shipping info")
-    }
-    else if (!billingValidation()) {
-      return alert("please complete your billing info")
-    }
-    else if(!cCValidation()) {
-     return alert('Please review your payment information')
-    }
-    else {
-      alert('thank you for your purchase!')
-    }
+  if ( !shippingValidation()) {
+    return alert("please complete your shipping info")
+  }
+  else if (!billingValidation()) {
+    return alert("please complete your billing info")
+  }
+  else if(!cCValidation()) {
+   return alert('Please review your payment information')
+  }
+  else {
+    alert('thank you for your purchase!')
+  }
 
 });
 
 function shippingValidation() {
-  $("#State-Shipping-1").on('change', function() {
-  console.log(this.selectedIndex);
-  });
-    if ( $('#First-Name-Shipping1').val() === '') {
-    return false;
-    }
-    else if( $('#Last-Name-Shipping1').val() === '') {
-    return false;
-    }
-    else if( $('#Address-Line-1-Shipping1').val() === '') {
-    return false;
-    }
-    else if($('#City-Shipping').val() === '') {
-    return false;
-    }
-    else if( $("#State-Shipping-1").attr('selectedIndex') > 0) {
-    return false;
-    }
-    else if ( $('#Zip-Shipping1').val().length < 5 || $('#Zip-Shipping1').val().length > 9) {
-    return false;
-    }
 
-return true;
-
+  if ( $('#First-Name-Shipping1').val() === '') {
+    return false;
+  }
+  else if( $('#Last-Name-Shipping1').val() === '') {
+    return false;
+  }
+  else if( $('#Address-Line-1-Shipping1').val() === '') {
+    return false;
+  }
+  else if($('#City-Shipping').val() === '') {
+    return false;
+  }
+  else if($("#State-Shipping-1").val() === '0') {
+    return false;
+  }
+  else if ( $('#Zip-Shipping1').val().length < 5 ||     $('#Zip-Shipping1').val().length > 9) {
+    return false;
+  }
+  return true;
 }
 
-//onchange to run check, then fill out
+
 
 $(".checkbox").on('click', function() {
 
@@ -72,10 +50,8 @@ $(".checkbox").on('click', function() {
     return alert("please complete your shipping info before copying info over into billing")
   }
   else {
-  copyToBilling()
+    copyToBilling()
   }
-
-
 });
 
 function copyToBilling() {
@@ -87,48 +63,43 @@ function copyToBilling() {
    $('#City-Billing').val($('#City-Shipping').val());
    $("#State-Billing-1").val($("#State-Shipping-1").val());
    $('#Zip-Billing1').val($('#Zip-Shipping1').val());
-
-// 1 this is the winning formula! $('#field2').val($('#field1').val());
-// 2 take a look at recs- it looks like shipping should just overwrite anything in billing?
 }
 
 function billingValidation() {
-  $("#State-Billing-1").on('change', function() {
-  console.log(this.selectedIndex);
-  });
-    if ( $('#First-Name-Billing1').val() === '') {
+
+  if ( $('#First-Name-Billing1').val() === '') {
     return false;
-    }
-    else if( $('#Last-Name-Billing1').val() === '') {
+  }
+  else if( $('#Last-Name-Billing1').val() === '') {
     return false;
-    }
-    else if( $('#Address-Line-1-Billing1').val() === '') {
+  }
+  else if( $('#Address-Line-1-Billing1').val() === '') {
     return false;
-    }
-    else if($('#City-Billing').val() === '') {
+  }
+  else if($('#City-Billing').val() === '') {
     return false;
-    }
-    else if( $("#State-Billing-1").attr('selectedIndex') > 0) {
+  }
+  else if( $("#State-Billing-1").val() === '0') {
     return false;
-    }
+  }
     else if ( $('#Zip-Billing1').val().length < 5 || $('#Zip-Billing1').val().length > 9) {
     return false;
-    }
+  }
 
-return true;
+  return true;
 
 }
 
 function cCValidation() {
-    if (!$('#Credit-Card1').val().length === 16) {
-     return false;
-    }
-    else if ($('#Expiration1').val() === '') {
-     return false;
-    }
-    else if ($('#CVC').val() === '') {
-     return false;
-    }
-    return true;
+  if (!$('#Credit-Card1').val().length === 16) {
+    return false;
+  }
+  else if ($('#Expiration1').val() === '') {
+    return false;
+  }
+  else if ($('#CVC').val() === '') {
+    return false;
+  }
+  return true;
 
 }
